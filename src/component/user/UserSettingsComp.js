bead81e7c54064c94b549b36f736fcff0f56d2ef
Jasur <zarrukhjurakulov474@gmail.com>
2022-01-20 14:31:01 +0500

    feat: upload avatar

--- src/component/user/UserSettingsComp.js
+++ src/component/user/UserSettingsComp.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Select from "react-dropdown-select";
 import "../../assets/scss/_settings.scss";
+import avatarLoc from "../../assets/images/user-g6f6f69a6e_1280.png";
+import { useDispatch } from "react-redux";
+import { updateProfileImage } from "../../redux/actions/userActions";
+import { BsCameraFill } from "react-icons/bs";
 
-const UserSettingsComp = () => {
+const UserSettingsComp = ({ userAvatar }) => {
+  const dispatch = useDispatch();
+
+  const handleUploadAvatar = (file, element) => {
+    const fileReader = new FileReader();
+    fileReader.addEventListener("load", function (e) {
+      element.src = e.target.value;
+    });
+    fileReader.readAsDataURL(file);
+    dispatch(updateProfileImage(file));
+  };
+
+  useEffect(() => {
+    dispatch({ type: "clear_success_message_avatar" });
+  }, []);
+
   return (
     <div className='user__settings__container'>
       <h1>Sozlamalar</h1>
       <div className='form'>
+        <img
+          src={userAvatar ? userAvatar : avatarLoc}
+          alt='avatar'
+          className='upload__avatar'
+        />
+        <div className='container__row'>
+          <label htmlFor='file' className='camera__icon'>
+            <BsCameraFill />
+          </label>
+          <input
+            type={"file"}
+            onChange={(e) =>
+              handleUploadAvatar(
+                e.target.files[0],
+                document.querySelectorAll("#avatarImage")[0]
+              )
+            }
+            id='file'
+            className='upload__img'
+          />
+        </div>
         <div className='container__row'>
           <div className='row__column'>
             <label htmlFor='name'>Ism</label>
@@ -16,6 +56,8 @@ const UserSettingsComp = () => {
             <label htmlFor='surname'>Familiya</label>
             <input type={"text"} id='surname' placeholder='Familiya' />
           </div>
+        </div>
+        <div className='container__row'>
           <div className='row__column'>
             <label htmlFor='phone__number'>Telefon raqam</label>
             <input
@@ -24,15 +66,14 @@ const UserSettingsComp = () => {
               placeholder='Telefon raqam'
             />
           </div>
-        </div>
-        <div className='container__row'>
           <div className='row__column'>
             <label htmlFor='region'>Viloyat</label>
             <select>
               <option disabled>Viloyatni tanlang</option>
             </select>
           </div>
-
+        </div>
+        <div className='container__row'>
           <div className='row__column'>
             <label htmlFor='district'>Tuman</label>
             <select>
--- src/pages/user/SettingsPage.js
+++ src/pages/user/SettingsPage.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../../assets/scss/_settings.scss";
+import { useSelector, useDispatch } from "react-redux";
 import UserSettingsComp from "../../component/user/UserSettingsComp";
+import { profileInfo } from "../../redux/actions/userActions";
 
 const SettingsPage = () => {
+  const dispatch = useDispatch();
+  const { avatar_url } = useSelector((state) => state.user.data);
+  useEffect(() => {
+    dispatch(profileInfo());
+  }, []);
   return (
     <div>
-      <UserSettingsComp />
+      <UserSettingsComp userAvatar={avatar_url} />
     </div>
   );
 };
