bf5cc244a0e58aa6d2a32e802378905d2fb6dffd
Jasur <zarrukhjurakulov474@gmail.com>
2022-01-20 14:35:10 +0500

    feat: common actions to redux

---
+++ src/redux/actions/commonActions.js
@@ -0,0 +1,19 @@
+import { ACTIONS } from "../reducers/appReducers";
+import requests from "../../helpers/requests";
+
+export const fetchLocations = () => (dispatch) => {
+  dispatch({ type: "fetch_locations_start" });
+
+  requests
+    .fetchLocations()
+    .then((data) => {
+      dispatch({ type: "fetch_locations_success", payload: data });
+    })
+    .catch(({ response }) => {
+      let message =
+        (response && response.data.message) ||
+        "SHaharlar royhatini yuklab bolmadi";
+
+      dispatch({ type: ACTIONS.FETCH_LOCATIONS_ERROR, payload: message });
+    });
+};
---
+++ src/redux/reducers/commonReducers.js
@@ -0,0 +1,44 @@
+const INITIAL_STATE = {
+  locations: [],
+  loading: false,
+  message: null,
+  category: [],
+};
+
+export const ACTIONS = {
+  FETCH_LOCATIONS_ERROR: "FETCH_LOCATIONS_ERROR",
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = INITIAL_STATE, { type, payload }) => {
+  switch (type) {
+    // auth
+    case "fetch_locations_start":
+      return { ...state, loading: true, message: "" };
+    case ACTIONS.FETCH_LOCATIONS_ERROR:
+      return { ...state, message: payload, loading: false };
+    case "fetch_locations_success":
+      return {
+        ...state,
+        message: payload.message,
+        loading: false,
+        locations: payload.data,
+      };
+
+    // Category
+    case "fetch_categorys_start":
+      return { ...state, loading: true, message: "" };
+    case "fetch_categorys_error":
+      return { ...state, message: payload, loading: false };
+    case "fetch_categorys_success":
+      return {
+        ...state,
+        message: payload.message,
+        loading: false,
+        category: payload.data,
+      };
+
+    default:
+      return state;
+  }
+};
