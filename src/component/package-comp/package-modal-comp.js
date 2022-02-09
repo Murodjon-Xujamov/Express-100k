cf0e61518ba7596ab362f1470761aa93ef543ede
Jasur <zarrukhjurakulov474@gmail.com>
2022-02-09 17:03:34 +0500

    style: package modal style

--- src/component/package-comp/package-modal-comp.js
+++ src/component/package-comp/package-modal-comp.js
@@ -3,6 +3,7 @@ import "../../assets/scss/packages.scss";
 import { Link } from "react-router-dom";
 import { IoMdClose } from "react-icons/io";
 import { FcPrint } from "react-icons/fc";
+import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
 import "react-dropdown/style.css";
 
 const PackageModalComp = ({ show, onClose, deliveryList, removeDelivery }) => {
@@ -29,88 +30,132 @@ const PackageModalComp = ({ show, onClose, deliveryList, removeDelivery }) => {
   return (
     <>
       {show ? (
-        <div className='delivery__modal'>
-          <div className='delivery__modal__header'>
-            <div className='delivery__modal__body__title'>
-              <IoMdClose
-                size={30}
-                className='delivery__modal__body__close_modal'
-                onClick={() => {
-                  onClose();
-                }}
-              />
-              <FcPrint size={30} className='delivery__modal__body__print' />
-            </div>
-          </div>
-
-          <div className='delivery__modal__body'>
-            <div className='delivery__modal__text__row'>
-              <span className='delivery__modal__text__left'>ID</span>
-              <span className='delivery__modal__text__right'>{id}</span>
-            </div>
-            <div className='delivery__modal__text__row'>
-              <span className='delivery__modal__text__left'>Qayerdan</span>
-              <span className='delivery__modal__text__right'>
-                {from_full_address}
-              </span>
-            </div>
-            <div className='delivery__modal__text__row'>
-              <span className='delivery__modal__text__left'>Qayerga</span>
-              <span className='delivery__modal__text__right'>
-                {to_full_address}
-              </span>
-            </div>
-            <div className='delivery__modal__text__row'>
-              <span className='delivery__modal__text__left'>Mahsulot</span>
-              <span className='delivery__modal__text__right'>{matter}</span>
-            </div>
-            <div className='delivery__modal__text__row'>
-              <span className='delivery__modal__text__left'>Xaridor</span>
-              <span className='delivery__modal__text__right'>
-                {recipient_name}
-              </span>
-            </div>
-            <div className='delivery__modal__text__row'>
-              <span className='delivery__modal__text__left'>
-                Xaridor raqami
-              </span>
-              <span className='delivery__modal__text__right'>
-                {recipient_phone}
-              </span>
-            </div>
-            <div className='delivery__modal__text__row'>
-              <span className='delivery__modal__text__left'>
-                Umumiy summasi
-              </span>
-              <span className='delivery__modal__text__right'>
-                {cash_amount}
-              </span>
-            </div>
-            <div className='delivery__modal__text__row'>
-              <span className='delivery__modal__text__left'>
-                Dostavka summasi
-              </span>
-              <span className='delivery__modal__text__right'>
-                {delivery_fee_amount}
-              </span>
-            </div>
-            <div className='delivery__modal__text__row'>
-              <span className='delivery__modal__text__left'>Vaqt</span>
-              <span className='delivery__modal__text__right'>{created_at}</span>
-            </div>
-            <div className='delivery__modal__text__row'>
-              <div></div>
-              <div>
-                <Link to={`/package/edit-package/${id}`}>
-                  <button className='btn bg-warning'>Edit</button>
-                </Link>
-                <button
-                  className='btn bg-danger text-white'
-                  onClick={() => deleteDeliveryData()}>
-                  Delete
-                </button>
+        <div className='container delivery__modal'>
+          <div className='col-12'>
+            <div className='row mt-5 p-5'>
+              <div className='col-12 col-lg-12 col-md-12 col-sm-12 text-end'>
+                <IoMdClose
+                  size={30}
+                  className='delivery__modal__body__close_modal '
+                  onClick={() => {
+                    onClose();
+                  }}
+                />
               </div>
-              <div></div>
+            </div>
+
+            <div className='container fw-bold'>
+              <div className='row'>
+                <div className='col-4 col-lg-4 col-md-4 col-sm-3'>
+                  <span className='text-bold'>ID</span>
+                </div>
+                <div className='col-8 col-lg-8 col-md-8 col-sm-9 text-end '>
+                  <span className='text-end'>{id}</span>
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col-4 col-lg-4 col-md-4 col-sm-3'>
+                  <span className=''>Qayerdan</span>
+                </div>
+                <div className='col-8 col-lg-8 col-md-8 col-sm-9 text-end'>
+                  <span className=''>{from_full_address}</span>
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col-4 col-lg-4 col-md-4 col-sm-3 '>
+                  <span className='delivery__modal__text__left'>Qayerga</span>
+                </div>
+                <div className='col-8 col-lg-8 col-md-8 col-sm-9 text-end'>
+                  <span className='delivery__modal__text__right'>
+                    {to_full_address}
+                  </span>
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col-4 col-lg-4 col-md-4 col-sm-3'>
+                  <span className='delivery__modal__text__left'>Mahsulot</span>
+                </div>
+                <div className='col-8 col-lg-8 col-md-8 col-sm-9 text-end'>
+                  <span className='delivery__modal__text__right'>{matter}</span>
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col-4 col-lg-4 col-md-4 col-sm-3'>
+                  <span className='delivery__modal__text__left'>Xaridor</span>
+                </div>
+                <div className='col-8 col-lg-8 col-md-8 col-sm-9 text-end'>
+                  <span className='delivery__modal__text__right'>
+                    {recipient_name}
+                  </span>
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col-4 col-lg-4 col-md-4 col-sm-3'>
+                  <span className='delivery__modal__text__left'>
+                    Xaridor raqami
+                  </span>
+                </div>
+                <div className='col-8 col-lg-8 col-md-8 col-sm-9 text-end'>
+                  <span className='delivery__modal__text__right'>
+                    {recipient_phone}
+                  </span>
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col-4 col-lg-4 col-md-4 col-sm-3'>
+                  <span className='delivery__modal__text__left'>
+                    Umumiy summasi
+                  </span>
+                </div>
+                <div className='col-8 col-lg-8 col-md-8 col-sm-9 text-end'>
+                  <span className='delivery__modal__text__right'>
+                    {cash_amount}
+                  </span>
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col-4 col-lg-4 col-md-4 col-sm-3'>
+                  <span className='delivery__modal__text__left'>
+                    Dostavka summasi
+                  </span>
+                </div>
+                <div className='col-8 col-lg-8 col-md-8 col-sm-9 text-end'>
+                  <span className='delivery__modal__text__right'>
+                    {delivery_fee_amount}
+                  </span>
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col-4 col-lg-4 col-md-4 col-sm-3'>
+                  <span className='delivery__modal__text__left'>Vaqt</span>
+                </div>
+                <div className='col-8 col-lg-8 col-md-8 col-sm-9 text-end'>
+                  <span className='delivery__modal__text__right'>
+                    {created_at}
+                  </span>
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col-12 col-lg-4 col-md-6 col-sm-12 mt-2'>
+                  <Link to={`/package/edit-package/${id}`}>
+                    <button className='btn bg-warning px-5 w-100'>
+                      <AiOutlineEdit size={20} /> Edit
+                    </button>
+                  </Link>
+                </div>
+                <div className='col-12 col-lg-4 col-md-6 col-sm-12 mt-2'>
+                  <button className='btn btn-info bg-info px-5 text-white w-100'>
+                    <FcPrint size={20} /> Print
+                  </button>
+                </div>
+                <div className='col-12 col-lg-4 col-md-6 col-sm-12 mt-2'>
+                  <button
+                    className='btn bg-danger text-white px-5 w-100'
+                    onClick={() => deleteDeliveryData()}>
+                    <AiOutlineDelete size={20} /> Delete
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
